e23ac1c2ec105c56a09447d51578eb2652cf5057
ely77 <santillaneleazar@gmail.com>
2025-01-19 19:55:45 +0800

    Add files via upload
    Refactored

---
+++ src/test/java/org/camunda/community/examples/BPMNTest/TestEchoTestProcess.java
@@ -0,0 +1,37 @@
+package org.camunda.community.examples.twitter;
+
+import static io.camunda.zeebe.spring.test.ZeebeTestThreadSupport.waitForProcessInstanceCompleted;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ZeebeSpringTest
+@SpringBootTest
+public class TestEchoTestProcess {
+
+  @Autowired ZeebeClient zeebeClient;
+
+  // @Autowired UserTaskService userTaskService;
+
+  @Test
+  void TestshouldRun() {
+    ProcessInstanceEvent process =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("Echo_Test_Process")
+            .latestVersion()
+            .send()
+            .join();
+    // userTaskService.complete(awaitUserTask().getKey(), JsonNodeFactory.instance.objectNode());
+    waitForProcessInstanceCompleted(process, Duration.ofSeconds(30));
+    BpmnAssert.assertThat(process)
+        .hasPassedElement("StartEvent_1", 1)
+        .hasPassedElement("Event_19as5zg", 1);
+  }
+}
